570ed76d8b742f38aa399cbd4ab39fe0b32a707a
Manuel Panichelli <panicmanu@gmail.com>
2023-08-28 21:03:11 -0300

    data de proof

--- prover/prover.hs
+++ prover/prover.hs
@@ -23,5 +23,50 @@ data Form =
 
 data Proof =
     PAx HypId
-    | PAndI Proof Proof -- <t, s>
-    | PAndE1 Form Proof 
+    -- A ^ B
+    | PAndI Proof -- A
+            Proof -- de B
+    | PAndE1 Form -- B
+             Proof -- de A
+    | PAndE2 Form -- A
+             Proof -- de B
+    -- A v B deduce C
+    | POrI1 Proof -- A
+    | POrI2 Proof -- B
+    | POrE Form -- A
+           Form -- B
+           Proof -- de A v B
+           HypId -- x:A
+           Proof -- de C asumiendo A
+           HypId -- x:B
+           Proof -- de C asumiendo B
+    -- A -> B
+    | PImpI HypId Proof
+    | PImpE Form -- A
+            Proof -- de A -> B
+            Proof -- de A
+    -- ¬ A = A -> bottom
+    | PNotI HypId -- x:A
+            Proof -- de bottom
+    | PNotE Form
+            Proof
+            Proof
+    | PTrueI
+    | PFalseE Proof
+    | PLEM
+    -- V x . A
+    | PForallI Proof -- de A
+    | PForallE VarId -- x
+               Form -- A (sin sust)
+               Proof -- de V x . A
+    -- E x . A deduce B
+    | PExistsI Term -- t
+               Proof -- de A con x reemplazado por t  
+    | PExistsE VarId -- x:A
+               Form -- A
+               Proof -- de E x. A
+
+data Env = EEmpty | EExtend Env HypId Form
+
+check :: Env -> Proof -> Form -> Bool
+check = undefined
