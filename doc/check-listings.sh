0d678813fa0b1e39a7a77144e3b21e0381a4941f
Manuel Panichelli <panicmanu@gmail.com>
2024-11-03 16:42:34 -0300

    Documento de tesis parte 1 (#15)
    - Deducción natural pulido y corregido
    - PPA interfaz pulido
    - PPA certifier versión 1
    
    El resto, todo draft.

---
+++ doc/check-listings.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIRECTORY="./listings"
+
+walk_dir() {
+    echo "walk $1"
+    for FILE in "$1"/*; do
+        if [ -f "$FILE" ] && [[ "$FILE" == *.ppa ]]; then
+            echo "ppa check $FILE"
+
+            ppa check "$FILE"
+        fi
+        if [ -d "$FILE" ]; then
+            walk_dir $FILE
+        fi
+    done
+}
+
+
+walk_dir $DIRECTORY
